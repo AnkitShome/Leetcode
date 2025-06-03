05ee35c00c9451aa527be2ede5732cf86888a065
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-06-03 19:12:04 +0530

    Time: 8 ms (37.02%), Space: 45.3 MB (19.71%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
@@ -1,45 +1,40 @@
 class Solution {
 public:
-    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
-        int n=status.size();
-        vector<int> vis(n);
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys,
+                   vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        int n = status.size();
+        vector<int> vis(n, 0), eaten(n, 0);
         queue<int> q;
-        vector<int> key(n);
-        for(auto i:initialBoxes)    {q.push(i);vis[i]++;}
 
-        int candy=0;
-
-        vector<int> eaten(n);
-
-        while(!q.empty()){
-            int box=q.front();
-            q.pop();
-            if(status[box]==0 and key[box]==0)  continue;
-            
-            if(vis[box]==1 and status[box]==1 and eaten[box]==0){
-                candy+=candies[box];eaten[box]=1;
-            }
-                
-            else if(vis[box]==1 and key[box]==1 and eaten[box]==0){
-                candy+=candies[box];
-                eaten[box]=1;
-            }
-
-            else    continue;
-
-            for(auto it:keys[box]){
-                key[it]=1;
-                if(eaten[it])  continue;
-                q.push(it);
-            }
-            for(auto it:containedBoxes[box]){
-                if(eaten[it]) continue;
-                vis[it]=1;
-                q.push(it);
-            }
-
-
+        for (int i : initialBoxes) {
+            q.push(i);
+            vis[i] = 1;
         }
+
+        int candy = 0;
+
+        while (!q.empty()) {
+            int box = q.front(); q.pop();
+
+            if (status[box] == 0 || eaten[box]) continue;
+
+            candy += candies[box];
+            eaten[box] = 1;
+
+            for (int k : keys[box]) {
+                status[k] = 1;
+                if (vis[k] && !eaten[k])
+                    q.push(k);
+            }
+
+            for (int b : containedBoxes[box]) {
+                if (!eaten[b]) {
+                    vis[b] = 1;
+                    q.push(b);
+                }
+            }
+        }
+
         return candy;
     }
 };
