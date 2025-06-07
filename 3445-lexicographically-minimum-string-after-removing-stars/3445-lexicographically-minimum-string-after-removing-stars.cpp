1cb1f8f600147f2e6d7d8d3a67ce7df0daec3f52
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-06-07 18:42:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string clearStars(string s) {
+        int n=s.size();
+        vector<int> don(n);
+        vector<set<int>> ind(26);
+        vector<int> freq;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='*')   {freq.push_back(i);continue;}
+            ind[s[i]-'a'].insert(i);
+        }
+        for(auto i:freq){
+            don[i]=1;
+            for(int j=0;j<26;j++){
+                auto it=ind[j].upper_bound(i);
+                if(it==ind[j].begin()){continue;}
+                it--;
+                int idx =*it;
+                ind[j].erase(it);
+                don[idx]=1;
+                break;
+            }
+        }
+        string res="";
+        for(int i=0;i<n;i++){
+            if(don[i])  continue;
+            res+=s[i];
+        }
+        return res;
+    }
+};
