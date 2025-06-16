79fd236ef3f3a0206bd9e7bb53da103869e0629f
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-06-16 23:56:58 +0530

    Time: 572 ms (5.81%), Space: 76.4 MB (21.84%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    map<string,int> mp;
+    set<string> mp;
     int f(int ind,int count,string s,vector<int>&dp){
         int n=s.size();
         if(ind==n){
@@ -13,21 +13,21 @@ public:
         for(int i=ind;i<n;i++){
             string temp=s.substr(ind,i-ind+1);
             if(mp.find(temp)==mp.end()) continue;
-            mp[temp]--;
             res=res | f(i+1,count+1,s,dp);
-            mp[temp]++;
         }
         return dp[ind]=res;
     }
 
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        for(auto i:words)   mp[i]++;
+        for(auto i:words)   mp.insert(i);
         vector<string> ans;
         for(auto s:words){
+            mp.erase(s);
             int n=s.size();
             vector<int> dp(n,-1);
             int possible=f(0,0,s,dp);
             if(possible)    ans.push_back(s);
+            mp.insert(s);
         }
         return ans;
     }
