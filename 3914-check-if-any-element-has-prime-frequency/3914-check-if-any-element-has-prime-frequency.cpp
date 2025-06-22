f36935e0c53bd6ff9a032142168287cf3fe9fb6f
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-06-22 13:11:35 +0530

    Time: 5 ms (10.24%), Space: 25.6 MB (5.76%) - LeetHub

--- 3914-check-if-any-element-has-prime-frequency/3914-check-if-any-element-has-prime-frequency.cpp
+++ 3914-check-if-any-element-has-prime-frequency/3914-check-if-any-element-has-prime-frequency.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
+    vector<int> prime;
+    vector<int> sieve(){
+        prime[0]=prime[1]=0;
+        for(int i=2;i*i<=100;i++){
+            for(int j=i*2;j<=100;j+=i){
+                prime[j]=0;
+            }
+        }
+        return prime;
+    }
     bool checkPrimeFrequency(vector<int>& nums) {
         map<int,int> freq;
         for(auto i:nums)    freq[i]++;
-        
+        prime.resize(101,1);
+        sieve();
         for(auto [m,n]:freq){
-            bool pro=1;
-            if(n==1)    pro=0;
-            for(int i=2;i<n;i++){
-                if(n%i==0)  pro=0;
-            }
-            cout<<m<<" "<<n<<endl;
-            if(pro) return true;
+            if(prime[n]==1) return true;
         }
         return false;
     }
