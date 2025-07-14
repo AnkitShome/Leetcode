055ad61a04739156d92e91692b79b7b7dfb87cfd
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-07-14 15:56:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,31 +1,51 @@
 class Solution {
 public:
-    vector<int> adj[200];
-    int vis[200];
-    int count=0;
-    void dfs(int node){
-        vis[node]=1;
-        for(auto it:adj[node]){
-            if(vis[it]) continue;
-            dfs(it);
+
+    class DSU{
+        vector<int> parent,size;
+        public:
+        DSU(int n){
+            parent.resize(n);
+            size.resize(n,1);
+            for(int i=0;i<n;i++)   parent[i]=i;
         }
-    }
+
+        int find(int u){
+            if(parent[u]==u)    return u;
+            return parent[u]=find(parent[u]);
+        }
+
+        void unite(int a,int b){
+            int pa=find(a);
+            int pb=find(b);
+            if(a==b)    return;
+            if(a<b) swap(a,b);
+            parent[b]=a;
+            if(size[a]==size[b])    size[a]++;
+        }
+
+    };
+
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
+        
+        DSU dsu(n);
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(isConnected[i][j]==1){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
+                    dsu.unite(i,j);
                 }
             }
         }
         int count=0;
+        set<int> st;
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i);
+            int par=dsu.find(i);
+            if(!st.count(par)){
+                st.insert(par);
                 count++;
-            } 
+            }
         }
         return count;
     }
