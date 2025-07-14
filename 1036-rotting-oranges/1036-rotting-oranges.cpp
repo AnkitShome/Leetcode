f830bc75459f8a8da87ca3fdc3eb020ce267356a
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-07-14 16:14:01 +0530

    Time: 3 ms (29.8%), Space: 17 MB (47.36%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int dx[4]={0,0,-1,1};
+    int dy[4]={-1,1,0,0};
+
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m));
+        int eva=0;
+        queue<array<int,3>> q;
+        
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1)   eva++;
+                if(grid[i][j]==2){
+                    q.push({i,j,0});
+                    vis[i][j]=1;
+                }
+            }
+
+        int ans=0;
+
+        while(!q.empty()){
+            auto [x,y,time]=q.front();
+            ans=max(ans,time);
+            q.pop();
+
+            for(int i=0;i<4;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                if(nx<0 or nx>=n or ny<0 or ny>=m or vis[nx][ny] or grid[nx][ny]!=1)
+                    continue;
+                
+                vis[nx][ny]=1;
+                eva--;
+                q.push({nx,ny,time+1});
+            }
+        }
+        if(eva) return -1;
+        return ans;
+    }
+};
