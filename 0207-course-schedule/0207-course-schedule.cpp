e4eeeb457caa9117a1c019e9a76425d87883f989
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-07-15 13:21:21 +0530

    Time: 12 ms (15.65%), Space: 20.1 MB (31.22%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,21 +1,5 @@
 class Solution {
 public:
-
-    bool dfs(int node,vector<int> &vis,vector<vector<int>>&adj,vector<int> &recstack){
-        vis[node]=1;
-        recstack[node]=1;
-        for(auto it:adj[node]){
-            if(!vis[it]){
-                if(dfs(it,vis,adj,recstack))    return true;
-            }
-            else if(recstack[it]){
-                return true;
-            }
-        }
-        recstack[node]=0;
-        return false;
-    }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int n=numCourses;
         vector<vector<int>> adj(n);
@@ -26,32 +10,25 @@ public:
             adj[v].push_back(u);
             indegree[u]++;
         }
-        vector<int> recstack(n);
-        vector<int> vis(n);
+
+        queue<int> q;
         for(int i=0;i<n;i++){
-            if(!vis[i]) if(dfs(i,vis,adj,recstack)) return false;
+            if(indegree[i]==0)  {q.push(i);}
         }
-        return true;
 
-        // queue<int> q;
-        // for(int i=0;i<n;i++){
-        //     if(indegree[i]==0)  {q.push(i);vis[i]=1;}
-        // }
+        vector<int> topo;
+        while(!q.empty()){
+            auto u=q.front();q.pop();
+            topo.push_back(u);
+            for(auto v:adj[u]){
+                indegree[v]--;
+                if(!indegree[v]){
+                    q.push(v);
+                }
+            }
+        }
 
-        // vector<int> topo;
-        // while(!q.empty()){
-        //     auto u=q.front();q.pop();
-        //     topo.push_back(u);
-        //     for(auto v:adj[u]){
-        //         if(vis[v])  continue;
-        //         indegree[v]--;
-        //         if(!indegree[v]){
-        //             q.push(v);
-        //             vis[v]=1;
-        //         }
-        //     }
-        // }
-        // if(topo.size()==n)  return true;
-        // return false;
+        if(topo.size()==n)  return true;
+        return false;
     }
 };
