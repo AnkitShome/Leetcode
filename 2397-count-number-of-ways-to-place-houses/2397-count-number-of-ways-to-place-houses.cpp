5914e1abd711338947f2a205496a6181aacc318c
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-07-24 14:09:34 +0530

    Time: 3 ms (80.98%), Space: 7.9 MB (79.51%) - LeetHub

--- 2397-count-number-of-ways-to-place-houses/2397-count-number-of-ways-to-place-houses.cpp
+++ 2397-count-number-of-ways-to-place-houses/2397-count-number-of-ways-to-place-houses.cpp
@@ -1,40 +1,13 @@
 class Solution {
 public:
-
-    int N;
-    int dp[10001][2][2];
-    const int mod=1e9+7;
-
-    int f(int i, int l, int r) {
-        if (i == N) return 1;
-        if (dp[i][l][r] != -1) return dp[i][l][r];
-
-        long long res = 0;
-
-        if (l && r) {
-            res = f(i + 1, 0, 0);
-        } 
-        else if (!l && r) {
-            res = f(i + 1, 1, 0);
-            res = (res + f(i + 1, 0, 0)) % mod;
-        } 
-        else if (l && !r) {
-            res = f(i + 1, 0, 0);
-            res = (res + f(i + 1, 0, 1)) % mod;
-        } 
-        else {
-            res = f(i + 1, 0, 0);
-            res = (res + f(i + 1, 0, 1)) % mod;
-            res = (res + f(i + 1, 1, 0)) % mod;
-            res = (res + f(i + 1, 1, 1)) % mod;
-        }
-
-        return dp[i][l][r] = (int)(res % mod);
-    }
-
     int countHousePlacements(int n) {
-        N=n;
-        memset(dp,-1,sizeof(dp));
-        return f(0,0,0);
+        const int mod = 1e9 + 7;
+        long long a = 1, b = 2;  
+        for (int i = 2; i <= n; ++i) {
+            long long c = (a + b) % mod;
+            a = b;
+            b = c;
+        }
+        return (b * b) % mod;
     }
 };
