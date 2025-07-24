6a8700bdf1ba49b66661f7f563dc9b13eceed39a
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-07-24 13:51:06 +0530

    Time: 0 ms (100%), Space: 86.4 MB (57.85%) - LeetHub

--- 1348-maximum-score-of-spliced-array/1348-maximum-score-of-spliced-array.cpp
+++ 1348-maximum-score-of-spliced-array/1348-maximum-score-of-spliced-array.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int n;
-    int dp[100001][3];
 
-    int f(int i,int state,vector<int> &nums1,vector<int> &nums2){
-        if(state==2 || i==n)    return 0;
-
-        if(dp[i][state]!=-1)  return dp[i][state];
-
-        int done=0,stick=0;
-        if(state==0){
-            stick=f(i+1,state,nums1,nums2);
-            done=nums2[i]-nums1[i]+f(i+1,1,nums1,nums2);
+    int kadane(vector<int>& nums1,vector<int>& nums2){
+        int cur=0,ans=0;
+        for(int i=0;i<n;i++){
+            int diff=nums2[i]-nums1[i];
+            cur=max(cur+diff,diff);
+            ans=max(ans,cur);
         }
-        else if(state==1){
-            stick=nums2[i]-nums1[i]+f(i+1,state,nums1,nums2);
-            done=f(i+1,2,nums1,nums2);
-        }
-
-        return dp[i][state]=max(stick,done);
+        return ans;
     }
 
     int maximumsSplicedArray(vector<int>& nums1, vector<int>& nums2) {
@@ -27,10 +18,8 @@ public:
         for(int i=0;i<n;i++){
             s+=nums1[i];t+=nums2[i];
         }
-        memset(dp,-1,sizeof(dp));
-        int a=f(0,0,nums1,nums2);
-        memset(dp,-1,sizeof(dp));
-        int b=f(0,0,nums2,nums1);
-        return max(s+a,t+b);
+        s+=kadane(nums1,nums2);
+        t+=kadane(nums2,nums1);
+        return max(s,t);
     }
 };
