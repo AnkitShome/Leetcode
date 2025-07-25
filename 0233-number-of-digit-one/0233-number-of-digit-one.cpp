2c6f4074d2bb98c6a595ded404f1c5c6e687cbb7
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-07-25 18:06:13 +0530

    Time: 0 ms (100%), Space: 8.1 MB (17.17%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.cpp
+++ 0233-number-of-digit-one/0233-number-of-digit-one.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
+    int dp[10][2][10][2]; 
 
-    int f(int pos,int tight,int started,int one,vector<int> &digits,int n,unordered_map<string,int> &dp){
-        if(pos==n){
-            if(!started)    return 0;
-            return one;
+    int f(int pos, int tight, int countOne, int started, const vector<int>& digits) {
+        if (pos == digits.size()) {
+            return countOne;
         }
 
-        string key=to_string(pos)+"_"+(tight?"1":"0")+"_"+(started?"1":"0")+"_"+to_string(one);
+        if (dp[pos][tight][countOne][started] != -1) return dp[pos][tight][countOne][started];
 
-        if(dp.find(key)!=dp.end()) return dp[key];
+        int limit = tight ? digits[pos] : 9;
+        int res = 0;
 
-        int limit=tight? digits[pos]:9;
-
-        int ans=0;
-
-        for(int d=0;d<=limit;d++){
-            int newTight=tight and (d==limit);
-            int newStarted=started or (d>0);
-            int newOne=one+(d==1?1:0);
-
-           ans += f(pos + 1, newTight, newStarted, newOne, digits, n, dp);
+        for (int d = 0; d <= limit; d++) {
+            int newTight = tight && (d == limit);
+            int newStarted = started || (d > 0);
+            int newCountOne = countOne + ((newStarted && d == 1) ? 1 : 0);
+            res += f(pos + 1, newTight, newCountOne, newStarted, digits);
         }
 
-        return dp[key]=ans;
-
+        return dp[pos][tight][countOne][started] = res;
     }
 
     int countDigitOne(int n) {
-        if(n<1) return 0;
-        string s=to_string(n);
+        if (n < 1) return 0;
+
         vector<int> digits;
-        int m=s.size();
-        for(int i=0;i<m;i++){
-            digits.push_back(s[i]-'0');
+        while (n) {
+            digits.push_back(n % 10);
+            n /= 10;
         }
-        unordered_map<string,int> dp;
-        return f(0,1,0,0,digits,m,dp);
-    }   
+        reverse(digits.begin(), digits.end());
+
+        memset(dp, -1, sizeof(dp));
+        return f(0, 1, 0, 0, digits);
+    }
 };
