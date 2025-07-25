1817038978a2266598d8ba97347e8e4a6ea4ad7f
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-07-25 15:25:38 +0530

    Time: 119 ms (62.15%), Space: 142.7 MB (46.15%) - LeetHub

---
+++ 3795-zero-array-transformation-iv/3795-zero-array-transformation-iv.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int n;
+    
+    int f(int ind,int i,int num,vector<vector<int>>& dp,vector<vector<int>>& queries){
+        if(num==0)  return ind;
+        if(ind==n)  return 1e9;
+
+        if(dp[ind][num]!=-1)    return dp[ind][num];
+
+        int skip=f(ind+1,i,num,dp,queries);
+        int take=1e9;
+        
+        int l = queries[ind][0];
+        int r = queries[ind][1];
+        int v = queries[ind][2];
+
+
+        if(num>=v and i>=l and i<=r){
+            take=f(ind+1,i,num-v,dp,queries);
+        }
+        return dp[ind][num]=min(take,skip);
+
+    }
+
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        n=queries.size();
+        int trophy=0;
+        for(int i=0;i<nums.size();i++){
+            int num=nums[i];
+            vector<vector<int>> dp(n,vector<int>(num+1,-1));
+            int res=f(0,i,num,dp,queries);
+            trophy=max(trophy,res);     
+            cout<<res<<endl;
+            if(trophy>=1e9) return -1;
+        }
+        return trophy;
+    }
+};
