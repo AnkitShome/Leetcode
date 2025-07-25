25391d14b46d228f2a0086f234e4e6b11c256027
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-07-25 18:02:59 +0530

    Time: 2 ms (4.49%), Space: 11 MB (5.42%) - LeetHub

---
+++ 0233-number-of-digit-one/0233-number-of-digit-one.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+
+    int f(int pos,int tight,int started,int one,vector<int> &digits,int n,unordered_map<string,int> &dp){
+        if(pos==n){
+            if(!started)    return 0;
+            return one;
+        }
+
+        string key=to_string(pos)+"_"+(tight?"1":"0")+"_"+(started?"1":"0")+"_"+to_string(one);
+
+        if(dp.find(key)!=dp.end()) return dp[key];
+
+        int limit=tight? digits[pos]:9;
+
+        int ans=0;
+
+        for(int d=0;d<=limit;d++){
+            int newTight=tight and (d==limit);
+
+            if(!started){
+                if(d==0){
+                    ans+=f(pos+1,newTight,0,0,digits,n,dp);
+                }
+                else{
+                    if(d==1){
+                        ans+=f(pos+1,newTight,1,one+1,digits,n,dp);
+                    }
+                    else{
+                        ans+=f(pos+1,newTight,1,one,digits,n,dp);
+                    }
+                }
+            }
+            else{
+                if(d==1){
+                    ans+=f(pos+1,newTight,1,one+1,digits,n,dp);
+                }
+                else{
+                    ans+=f(pos+1,newTight,1,one,digits,n,dp);
+                }
+            }
+        }
+
+        return dp[key]=ans;
+
+    }
+
+    int countDigitOne(int n) {
+        if(n<1) return 0;
+        string s=to_string(n);
+        vector<int> digits;
+        int m=s.size();
+        for(int i=0;i<m;i++){
+            digits.push_back(s[i]-'0');
+        }
+        unordered_map<string,int> dp;
+        return f(0,1,0,0,digits,m,dp);
+    }   
+};
