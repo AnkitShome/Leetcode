0bdfead4a7274059017587a51b07076a30091497
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-07-29 19:13:57 +0530

    Time: 7 ms (67.77%), Space: 11.5 MB (83.36%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,14 +5,14 @@ public:
         int i=0,j=0;
         int ans=0;
         vector<int> vis(256,-1);
-        while(i<n){
-            if(vis[s[i]]!=-1){
-                int l=vis[s[i]];
-                if(l>=j) j=l+1;
+        while(j<n){
+            if(vis[s[j]]!=-1){
+                int l=vis[s[j]];
+                i=max(i,l+1);
             }
-            vis[s[i]]=i;
-            ans=max(ans,i-j+1);
-            i++;
+            vis[s[j]]=j;
+            ans=max(ans,j-i+1);
+            j++;
         }
         return ans;
     }
