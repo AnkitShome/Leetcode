abce2ebcdcf9e803a391cf9aa697f267e6f7a38e
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-08-03 02:19:29 +0530

    Time: 4 ms (47.68%), Space: 24 MB (5.33%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -27,11 +27,11 @@ public:
 
         while(l1!=NULL and l2!=NULL){
             if(l1->val < l2->val){
-                cur->next=new ListNode(l1->val);
+                cur->next=l1;
                 l1=l1->next;
             }
             else{
-                cur->next=new ListNode(l2->val);
+                cur->next=l2;
                 l2=l2->next;
             }
             cur=cur->next;
