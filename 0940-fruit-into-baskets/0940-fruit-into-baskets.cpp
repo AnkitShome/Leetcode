595424b77ff36305f78bc42c41897823fe59ea26
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-08-04 11:06:02 +0530

    Time: 33 ms (79.3%), Space: 75.1 MB (57.1%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -9,15 +9,12 @@ public:
         int ans=0;
 
         for(r=0;r<n;r++){
-            // for(auto [i,j]:st)  {cout<<i<<" "<<j<<endl;}
-            // cout<<endl;
-            while(st.size()==2 and (st.find(fruits[r])==st.end())){
+            st[fruits[r]]++;
+            while(st.size()>2){
                 st[fruits[l]]--;
-                // cout<<st[fruits[l]]<<" x "<<endl;
                 if(st[fruits[l]]==0)    st.erase(fruits[l]);
                 l++;
             }
-            st[fruits[r]]++;
             int present=r-l+1;
             ans=max(ans,present);
         }
