1725118f091daac3247b7ce07b3b054a72f8829f
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-08-07 16:41:03 +0530

    Time: 232 ms (41.68%), Space: 41.4 MB (41.15%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -15,7 +15,18 @@ public:
     }
     int lengthOfLIS(vector<int>& nums) {
         n=nums.size();
-        memset(dp,-1,sizeof(dp));
-        return f(0,-1,nums);
+        memset(dp,0,sizeof(dp));
+        // return f(0,-1,nums);
+        
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prev=ind-1;prev>=-1;prev--){
+                int not_take=dp[ind+1][prev+1];
+                int take=0;
+                if(prev==-1 or nums[ind]>nums[prev])
+                    take=1+dp[ind+1][ind+1];
+                dp[ind][prev+1]=max(take,not_take);
+            }
+        }
+        return dp[0][0];
     }
 };
