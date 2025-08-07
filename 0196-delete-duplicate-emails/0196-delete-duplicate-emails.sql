c7385541f2e66c319c6d572e750e91f05922f59c
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-08-07 19:43:45 +0530

    Time: 520 ms (94.45%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,1 +1,8 @@
-delete p1 from Person p1 join Person p2 on p1.email=p2.email and p1.id>p2.id
+DELETE FROM Person
+WHERE id NOT IN (
+  SELECT min_id FROM (
+    SELECT MIN(id) AS min_id
+    FROM Person
+    GROUP BY email
+  ) AS t
+);
