70eadbaf6e230473668a920b1ff9447e8744bf07
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-08-18 17:25:55 +0530

    Time: 27 ms (5.79%), Space: 33.7 MB (5.17%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,29 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <string>
-using namespace std;
-
 class Solution {
 public:
-    vector<string> res;
+    unordered_set<string> ans;
+    int n;
 
-    void backtrack(string cur, int open, int close, int n) {
-        if (cur.size() == 2 * n) {
-            res.push_back(cur);
+    void recur(int ind,int val,int now,string cur){
+        if(ind==n){
+            if(val==0){
+                ans.insert(cur);
+            }
             return;
         }
-
-        if (open < n) { 
-            backtrack(cur + "(", open + 1, close, n);
+        if(now==0){
+            cur+='(';
+            val++;
+            recur(ind+1,val,0,cur);
+            cur.pop_back();
+            cur+='(';
+            recur(ind+1,val,1,cur);
+            cur.pop_back();
         }
-        if (close < open) { 
-            backtrack(cur + ")", open, close + 1, n);
+        else{
+            if(val<=0)  return;
+            cur+=')';
+            val--;
+            recur(ind+1,val,0,cur);
+            cur.pop_back();
+            cur+=')';
+            recur(ind+1,val,1,cur);
+            cur.pop_back();
         }
     }
 
-    vector<string> generateParenthesis(int n) {
-        res.clear();
-        backtrack("", 0, 0, n);
+    vector<string> generateParenthesis(int m) {
+        n=2*m;
+        cout<<n<<endl;
+        string cur="";
+        recur(0,0,0,cur);
+        vector<string> res;
+        for(auto i:ans) res.push_back(i);
         return res;
     }
 };
