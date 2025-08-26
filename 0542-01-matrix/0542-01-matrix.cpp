ca9477330c2dad063008a5063b76ad04de37f779
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-08-26 17:04:12 +0530

    Time: 15 ms (74.83%), Space: 36 MB (63.65%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,41 +1,36 @@
 class Solution {
 public:
-    int dx[4]={0,0,-1,1};
-    int dy[4]={-1,1,0,0};
-
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        
+        int n=mat.size(),m=mat[0].size();
+        vector<pair<int,int>> dirs={{-1,0},{1,0},{0,-1},{0,1}};
         queue<array<int,3>> q;
-        vector<vector<int>> dist(n,vector<int>(m));
+        vector<vector<int>> dist(n,vector<int>(m,1e9));
 
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(mat[i][j]==0)    {
-                    mat[i][j]=-1;
-                    q.push({i,j,0});
+                if(mat[i][j]==0){
+                    q.push({0,i,j});
                     dist[i][j]=0;
                 }
             }
         }
+
         while(!q.empty()){
-            auto [x,y,dis]=q.front();
+            auto [d,x,y]=q.front();
             q.pop();
 
-            for(int i=0;i<4;i++){
-                int nx=x+dx[i];
-                int ny=y+dy[i];
-                if(nx<0 or nx>=n or ny<0 or ny>=m or mat[nx][ny]==-1)
-                    continue;
-                if(mat[nx][ny]==1){
-                    mat[nx][ny]=-1;
-                    q.push({nx,ny,dis+1});
-                    dist[nx][ny]=dis+1;
-                }
-            }  
+            for(auto [dx,dy]:dirs){
+                int nx=x+dx;
+                int ny=y+dy;
+                if(min(nx,ny)<0 or nx>=n or ny>=m)  continue;
+                if(mat[nx][ny]==0) continue;
+                if(dist[nx][ny]<=d+1)   continue;
+                dist[nx][ny]=d+1;
+                q.push({d+1,nx,ny});
+            }   
         }
 
         return dist;
+        
     }
 };
