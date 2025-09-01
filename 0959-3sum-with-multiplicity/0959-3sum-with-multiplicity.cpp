b3cf5e22c7c0559046c44aeca064bb6a01ee7341
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-09-02 00:50:30 +0530

    Time: 63 ms (36.51%), Space: 14.1 MB (74.68%) - LeetHub

---
+++ 0959-3sum-with-multiplicity/0959-3sum-with-multiplicity.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    const int mod=1e9+7;
+    int threeSumMulti(vector<int>& arr, int target) {
+        int n=arr.size();
+        sort(arr.begin(),arr.end());
+        long long count=0;
+        for(int i=0;i<n;i++){
+            int l=i+1,r=n-1;
+            while(l<r){
+                if(arr[l]+arr[r]+arr[i]==target){
+                    if(arr[l]!=arr[r]){
+                        int left=1,right=1;
+                        while(l+1<r and arr[l]==arr[l+1]){
+                            left++;
+                            l++;
+                        }
+                        while(r-1>l and arr[r-1]==arr[r]){
+                            right++;
+                            r--;
+                        }
+                        count+=(left%mod*right%mod)%mod;
+                        l++;
+                    }
+                    else{
+                        int k=(r-l+1);
+                        count+=(k*(k-1))/2;
+                        count%=mod;
+                        break;
+                    }
+                    
+                }
+                else if(arr[l]+arr[r]+arr[i]<target){
+                    l++;
+                }
+                else{
+                    r--;
+                }
+            }
+        }
+        return count%mod;
+    }
+};
