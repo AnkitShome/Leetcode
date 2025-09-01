f4e35417c8ada764bdb6d0a4a5c50c6c16f32b0c
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-09-01 14:54:26 +0530

    Time: 78 ms (48.5%), Space: 29 MB (14.67%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.cpp
@@ -3,24 +3,17 @@ public:
     int n;
     unordered_map<int,bool> dp;
 
-    string encode(int sum,int k){
-        string res="";
-        res+=to_string(sum)+"_";
-        res+=to_string(k)+"_";
-        return res;
-    }
-
     bool f(int sum,int bit,int k,int target,vector<int> &nums){
         if(k==0)    return true;
-        if(sum==target){
-            return f(0,bit,k-1,target,nums);
-        }
 
         if(dp.count(bit))   return dp[bit];
 
         for(int i=0;i<n;i++){
-            if(!(bit & (1<<i)) and sum+nums[i]<=target){
-                if(f(sum+nums[i],bit | (1<<i), k, target,nums)) return dp[bit]=true;
+            if(!(bit & (1<<i))){
+                if(sum+nums[i]==target)
+                    if(f(0,bit | (1<<i),k-1 ,target,nums))  return dp[bit]=true;
+                if(sum+nums[i]<target)
+                    if(f(sum+nums[i],bit | (1<<i), k, target,nums)) return dp[bit]=true;
             }
         }
         return dp[bit]=false;
