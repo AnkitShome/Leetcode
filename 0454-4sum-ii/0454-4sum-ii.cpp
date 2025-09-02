0591017cc8101327cabdd27a6c43066765cb6038
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-09-02 12:26:41 +0530

    Time: 88 ms (81.37%), Space: 28 MB (85.04%) - LeetHub

---
+++ 0454-4sum-ii/0454-4sum-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        int n=nums1.size();
+        unordered_map<int,int> mp;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        sort(nums3.begin(),nums3.end());
+        sort(nums4.begin(),nums4.end());
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                mp[nums3[i]+nums4[j]]++;
+            }
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int sum=nums1[i]+nums2[j];
+                if(mp.count(-sum)){
+                    ans+=mp[-sum];
+                
+                }
+            }
+        }
+        return ans;
+    }
+};
