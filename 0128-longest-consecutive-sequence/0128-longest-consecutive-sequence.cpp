3d39cb2af9c36bf9962e0f20b772b2c3d096b448
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-09-13 00:51:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,54 +1,24 @@
 class Solution {
 public:
-    class DSU{
-        public:
-        unordered_map<int,int> parent,size;
-        
-        int findPar(int node){
-            if(node==parent[node])  return node;
-            return parent[node]=findPar(parent[node]);
-        }
-        
-        void unite(int a,int b){
-            a=findPar(a);
-            b=findPar(b);
-            if(a==b)    return;
-            if(size[a]<size[b]) swap(a,b);
-            parent[b]=a;
-            size[a]+=size[b];
-        }
-
-        void add(int a){
-            if(!parent.count(a)){
-                parent[a]=a;
-                size[a]=1;
-            }
-        }
-        
-    };
-    
+  
     
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> s(nums.begin(),nums.end());
-        
-        DSU dsu;
-        
-        for(auto x:s){
-            dsu.add(x);
-            if(dsu.parent.count(x+1)){
-                dsu.unite(x,x+1);
-            }
-            if(dsu.parent.count(x-1)){
-                dsu.unite(x,x-1);
-            }
+        int n=nums.size();
+        unordered_map<int,int> vis;
+        for(int i=0;i<n;i++){
+            vis[nums[i]]=1;
         }
-
         int ans=0;
-
-        for(auto i:s){
-            ans=max(ans,dsu.size[dsu.findPar(i)]);
+        for(int i=0;i<n;i++){
+            int count=1;
+            if(vis[nums[i]]==2)   continue;
+            int next=nums[i]+1;
+            while(vis[next]==1){
+                count++;
+                next=next+1;
+            }
+            ans=max(ans,count);
         }
-
         return ans;
     }
 };
