f803c4cf6b89d1ac61579b3606098b91f9122272
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2025-09-13 20:44:35 +0530

    Time: 22 ms (5.15%), Space: 12.8 MB (60.66%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -3,30 +3,32 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         stack<int> st;
         int n=nums2.size();
-        vector<int> NGE(n,-1);
-       
         unordered_map<int,int> mp;
-        for(auto i:nums1){
-            for(int j=0;j<n;j++){
-                if(nums2[j]==i) mp[i]=j;
+        
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+                if(nums1[i]==nums2[j])  mp[i]=j;
             }
         }
 
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() and nums2[st.top()]<=nums2[i]){
+        vector<int> NGE(n,-1);
+
+        for(int i=0;i<n;i++){
+            while(!st.empty() and nums2[st.top()]<nums2[i]){
+                NGE[st.top()]=i;
                 st.pop();
             }
-            if(!st.empty()) NGE[i]=st.top();
             st.push(i);
         }
-
-        for(auto i:NGE) cout<<i<<" ";cout<<endl;
-
         vector<int> ans;
-        for(auto i:nums1){
-            if(NGE[mp[i]]==-1)   ans.push_back(-1);
-            else    ans.push_back(nums2[NGE[mp[i]]]);
+        
+        for(int i=0;i<nums1.size();i++){
+            if(!mp.count(i))    {ans.push_back(-1);continue;}  
+            if(NGE[mp[i]]!=-1)  
+                ans.push_back(nums2[NGE[mp[i]]]);
+            else    ans.push_back(-1);
         }
         return ans;
+        
     }
 };
