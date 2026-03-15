542119658b58a2e84d591b8da5766de2abd84e01
Ankit Shome <154817490+AnkitShome@users.noreply.github.com>
2026-03-15 14:00:13 +0530

    Time: 1 ms (49.53%), Space: 16.7 MB (15.96%) - LeetHub

---
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        int ans=0;
+        for(auto i:n){
+            ans=max(ans,i-'0');
+        }        
+        return ans;
+    }
+};
+
+
+
